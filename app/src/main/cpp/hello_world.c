848cd609e5657aa40ff1a122a87829b1f4a1a5a5
Maria Zhang <mariazhang@squareup.com>
2020-05-27 18:06:17 -0400

    fork() test

---
+++ app/src/main/cpp/hello_world.c
@@ -0,0 +1,40 @@
+#include <jni.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <android/log.h>
+#include <pthread.h>
+
+#define  LOG_TAG    "debug"
+#define  LOG_printf(...)  __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+
+void *threadFork(void *vargp)
+{
+    LOG_printf("printing from thread \n");
+
+    pid_t result = fork();
+    LOG_printf("result :%d \n", result);
+
+    LOG_printf("pid: %d \n", getpid());
+    LOG_printf(" Parent pid: %d \n", getppid());
+
+   // sleep(10000000);
+
+    return NULL;
+}
+
+JNIEXPORT jstring JNICALL
+Java_com_example_ndktest_MainActivity_helloWorld(
+        JNIEnv* env,
+        jobject obj) {
+
+    char text[] = "c string";
+
+    size_t pagesize = getpagesize();
+    LOG_printf("System page size: %zu bytes \n", pagesize);
+
+    pthread_t thread_id;
+    pthread_create(&thread_id, NULL, threadFork, NULL);
+    pthread_join(thread_id, NULL);
+
+    return (*env)->NewStringUTF(env, text);
+}
---
+++ app/src/main/java/com/example/ndktest/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.ndktest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+        System.loadLibrary("hello_world");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Example of a call to a native method
+        TextView tv = findViewById(R.id.sample_text);
+        //tv.setText(stringFromJNI());
+        tv.setText(helloWorld());
+        Log.i("test", helloWorld());
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+    public native String helloWorld();
+}
